6a069aa086b21aa2a0d4de20dc00bf18b0ad1e1f
willian <will.s.peixoto@gmail.com>
2021-12-03 13:52:53 -0300

    removed yml read using env

--- secrets-manager-extension/handle-files.js
+++ secrets-manager-extension/handle-files.js
@@ -1,41 +1,20 @@
-const yaml = require('js-yaml')
 const fs = require('fs');
-const path = require('path');
-
-module.exports.readConfigFile = async () => new Promise((resolve, reject) => {
-  console.log('[EXTENSION], debugging checking file');
-  try {
-    console.log(path.resolve("/var/task/secrets-config.yaml"));
-    const config = yaml.load(fs.readFileSync('/var/task/secrets-config.yaml', 'utf8', function (err, data) {
-      if (err)
-        console.log('error', err)
-      reject(err);
-    }));
-    console.log('config', config)
-    resolve(config);
-  } catch (e) {
-    console.log('[extension] error: ' + e);
-  }
-});
+//const path = require('path');
 
 module.exports.writeCredentials = async (key, filename) => new Promise((resolve, reject) => {
   console.log('[extension] filename: ' + filename);
   const fullpath = `/tmp/${filename}`;
-  const data = `{"credentials":${key}}`;
-  console.log('fullpath', fullpath)
   try {
-
     if (fs.existsSync(fullpath)) {
       console.log('[extension]file exist => ' + filename);
       resolve(true);
     }
     else {
-      fs.writeFile(`${fullpath}`, data, 'utf8', function (err) {
+      fs.writeFile(`${fullpath}`, key, 'utf8', function (err) {
         if (err) {
           console.log('[extension] error to write in the file');
           reject(err);
         }
-        //console.log('[extension] file was saved!');
         resolve(true);
       });
     }
@@ -45,7 +24,7 @@ module.exports.writeCredentials = async (key, filename) => new Promise((resolve,
 });
 
 module.exports.readFolder = async function () {
-  console.log('[EXTENSION], debugging checking files exists')
+  //console.log('[extension], debugging checking files exists')
   try {
     fs.readdir('/tmp/', (err, files) => {
       files.forEach(file => {
